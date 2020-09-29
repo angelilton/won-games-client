80a65cb0f0ce596091ce22cf16dc317fd5aed491
angelilton <epifanio.rds@gmail.com>
2020-09-29 22:16:49 +0100

    add option to size on logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -17,4 +17,18 @@ describe('<Logo />', () => {
       color: '#030517'
     })
   })
+
+  it('should render logo with default size', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
 })
