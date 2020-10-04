828b69aa668468974acff7dddbe819afe62e50b7
angelilton <epifanio.rds@gmail.com>
2020-10-04 23:40:29 +0100

    Add Button support for icons

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -1,4 +1,5 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
 import Button from '.'
 
 export default {
@@ -7,6 +8,9 @@ export default {
   argTypes: {
     children: {
       type: 'string'
+    },
+    icon: {
+      type: ''
     }
   }
 } as Meta
@@ -15,4 +19,12 @@ export const Default: Story = (args) => <Button {...args} />
 
 Default.args = {
   children: 'Buy Now'
+}
+
+export const widthIcon: Story = (args) => <Button {...args} />
+
+widthIcon.args = {
+  size: 'small',
+  children: 'Buy Now',
+  icon: <AddShoppingCart />
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,7 +1,9 @@
 import styled, { css, DefaultTheme } from 'styled-components'
 import { ButtonProps } from '.'
 
-type WrapperProps = Pick<ButtonProps, 'size' | 'fullWidth'>
+type WrapperProps = {
+  hasIcon: boolean
+} & Pick<ButtonProps, 'size' | 'fullWidth'>
 
 const wrapperModifiers = {
   fullWidth: () => css`
@@ -12,29 +14,53 @@ const wrapperModifiers = {
     height: 3rem;
     font-size: ${theme.font.sizes.xsmall};
     padding: ${theme.spacings.xxsmall};
+
+    svg {
+      width: 1.2rem;
+    }
   `,
 
   medium: (theme: DefaultTheme) => css`
     height: 4rem;
     font-size: ${theme.font.sizes.small};
     padding: ${theme.spacings.xxsmall} ${theme.spacings.medium};
+
+    svg {
+      width: 1.3rem;
+    }
   `,
 
   large: (theme: DefaultTheme) => css`
     height: 5rem;
     font-size: ${theme.font.sizes.medium};
     padding: ${theme.spacings.xxsmall} ${theme.spacings.xlarge};
+
+    svg {
+      width: 1.5rem;
+    }
+  `,
+
+  widthIcon: (theme: DefaultTheme) => css`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+
+    svg + span {
+      margin-left: ${theme.spacings.xxsmall};
+    }
   `
 }
 
 export const Wrapper = styled.button<WrapperProps>`
-  ${({ theme, size, fullWidth }) => css`
+  ${({ theme, size, fullWidth, hasIcon }) => css`
     background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
     color: ${theme.colors.white};
     border: 0;
+    cursor: pointer;
     border-radius: ${theme.border.radius};
 
-    ${!!size && wrapperModifiers[size](theme)}
-    ${!!fullWidth && wrapperModifiers.fullWidth()}
+    ${!!size && wrapperModifiers[size](theme)};
+    ${!!fullWidth && wrapperModifiers.fullWidth()};
+    ${!!hasIcon && wrapperModifiers.widthIcon(theme)};
   `}
 `
