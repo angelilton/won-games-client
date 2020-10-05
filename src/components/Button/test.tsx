809186c5c01f753d002a694bc0733851bd46aa03
angelilton <epifanio.rds@gmail.com>
2020-10-05 22:43:49 +0100

    add Button tests

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,13 +1,54 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
 
 import Button from '.'
 
 describe('<Button />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Button />)
+  it('should render the medium size by default', () => {
+    renderWithTheme(<Button>Buy Now</Button>)
 
-    expect(screen.getByRole('heading', { name: /Button/i })).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      height: '4rem',
+      fontSize: '1.4rem',
+      padding: '0.8rem 3.2rem'
+    })
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render the small size', () => {
+    renderWithTheme(<Button size="small">Buy Now</Button>)
+
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      height: '3rem',
+      fontSize: '1.2rem',
+      padding: '0.8rem'
+    })
+  })
+
+  it('should render the large size', () => {
+    renderWithTheme(<Button size="large">Buy Now</Button>)
+
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      height: '5rem',
+      fontSize: '1.6rem',
+      padding: '0.8rem 4.8rem'
+    })
+  })
+
+  it('should render  a fullWidth version', () => {
+    renderWithTheme(<Button fullWidth>Buy Now</Button>)
+
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      width: '100%'
+    })
+  })
+
+  it('should render an icon version', () => {
+    renderWithTheme(
+      <Button icon={<AddShoppingCart data-testid="icon" />}>Buy now</Button>
+    )
+
+    expect(screen.getByText(/buy now/i)).toBeInTheDocument()
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
   })
 })
