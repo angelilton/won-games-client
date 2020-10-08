a7fdc417990d161f579fc36f2559a98f3a6076c1
angelilton <epifanio.rds@gmail.com>
2020-10-08 23:06:21 +0100

    Add logic open/close to menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,30 +1,40 @@
+import { useState } from 'react'
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 
 import Logo from 'components/Logo'
 import * as S from './styles'
 
-const Menu = () => (
-  <S.Wrapper>
-    <S.IconWrapper>
-      <MenuIcon aria-label="open menu" />
-    </S.IconWrapper>
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
 
-    <S.LogoWrapper>
-      <Logo hideOnMobile />
-    </S.LogoWrapper>
-
-    <S.MenuGroup>
-      <S.IconWrapper>
-        <SearchIcon aria-label="Search" />
+  return (
+    <S.Wrapper>
+      <S.IconWrapper onClick={() => setIsOpen(true)}>
+        <MenuIcon aria-label="open menu" />
       </S.IconWrapper>
 
-      <S.IconWrapper>
-        <ShoppingCartIcon aria-label="open shopping cart" />
-      </S.IconWrapper>
-    </S.MenuGroup>
-  </S.Wrapper>
-)
+      <S.LogoWrapper>
+        <Logo hideOnMobile />
+      </S.LogoWrapper>
+
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </S.IconWrapper>
+
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="open shopping cart" />
+        </S.IconWrapper>
+      </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="close menu" onClick={() => setIsOpen(false)} />
+      </S.MenuFull>
+    </S.Wrapper>
+  )
+}
 
 export default Menu
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -38,3 +38,12 @@ export const IconWrapper = styled.div`
     height: 2.4rem;
   `}
 `
+type MenuFullProps = {
+  isOpen: boolean
+}
+
+export const MenuFull = styled.nav<MenuFullProps>`
+  ${({ isOpen }) => css`
+    opacity: ${isOpen ? 1 : 0};
+  `}
+`
