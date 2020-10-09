44cc885c83bc6044f5e219a8c990ef9931281c42
angelilton <epifanio.rds@gmail.com>
2020-10-09 13:51:09 +0100

    Add styles to MenuFull

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -32,6 +32,10 @@ const Menu = () => {
 
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="close menu" onClick={() => setIsOpen(false)} />
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explorer</S.MenuLink>
+        </S.MenuNav>
       </S.MenuFull>
     </S.Wrapper>
   )
