5b50dddc95623024d19397e871b35f7c5ce42f2d
angelilton <epifanio.rds@gmail.com>
2021-03-15 13:12:43 -0400

    Add Ribbon to Banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,4 +1,6 @@
 import Button from 'components/Button'
+import Ribbon, { RibbonSizes } from 'components/Ribbon'
+import { MainColors } from 'types/types'
 import * as S from './styles'
 
 export type BannerProps = {
@@ -7,6 +9,9 @@ export type BannerProps = {
   subtitle: string
   buttonLabel: string
   buttonLink: string
+  ribbon?: React.ReactNode
+  ribbonSize?: RibbonSizes
+  ribbonColor?: MainColors
 }
 
 const Banner = ({
@@ -14,10 +19,20 @@ const Banner = ({
   title,
   subtitle,
   buttonLabel,
-  buttonLink
+  buttonLink,
+  ribbon,
+  ribbonSize = 'normal',
+  ribbonColor = 'primary'
 }: BannerProps) => (
   <S.Wrapper>
+    {!!ribbon && (
+      <Ribbon size={ribbonSize} color={ribbonColor}>
+        {ribbon}
+      </Ribbon>
+    )}
+
     <S.Image src={img} role="img" aria-label={title} />
+
     <S.Caption>
       <S.Title>{title}</S.Title>
       <S.Subtitle dangerouslySetInnerHTML={{ __html: subtitle }} />
