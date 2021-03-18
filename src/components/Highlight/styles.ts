3a8e02920594f11999de3b44b3f53e2bb0a81e38
angelilton <epifanio.rds@gmail.com>
2021-03-17 22:19:39 -0400

    Add highlight alignment

--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -2,15 +2,33 @@ import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 import { HighlightProps } from '.'
 
-type WrapperProps = Pick<HighlightProps, 'backgroundImage'>
+type WrapperProps = Pick<HighlightProps, 'backgroundImage' | 'alignment'>
+
+const wrapperModifiers = {
+  right: () => css`
+    grid-template-areas: 'floatImage content';
+    grid-template-columns: 1.3fr 2fr;
+
+    ${Content} {
+      text-align: right;
+    }
+  `,
+
+  left: () => css`
+    grid-template-areas: 'content floatImage';
+    grid-template-columns: 2fr 1.3fr;
+
+    ${FloatImage} {
+      justify-self: end;
+    }
+  `
+}
 
 export const Wrapper = styled.section<WrapperProps>`
-  ${({ backgroundImage }) => css`
+  ${({ backgroundImage, alignment }) => css`
     position: relative;
     height: 23rem;
     display: grid;
-    grid-template-areas: 'floatimage content';
-    grid-template-columns: 1.3fr 2fr;
 
     background-image: url(${backgroundImage});
     background-position: center center;
@@ -27,15 +45,19 @@ export const Wrapper = styled.section<WrapperProps>`
     ${media.greaterThan('medium')`
      height: 32rem;
     `}
+
+    //alinha o text para a direita ou esquerda
+    ${wrapperModifiers[alignment!]()}
   `}
 `
+
 export const FloatImage = styled.img`
   ${({ theme }) => css`
-    grid-area: floatimage;
+    grid-area: floatImage;
     z-index: ${theme.layers.base};
     max-height: 23rem;
     max-width: 100%;
-    align-self: flex-end;
+    align-self: end;
 
     ${media.greaterThan('medium')`
      max-height: 32rem;
@@ -47,7 +69,6 @@ export const Content = styled.div`
   ${({ theme }) => css`
     grid-area: content;
     z-index: ${theme.layers.base};
-    text-align: right;
     padding: ${theme.spacings.xsmall};
 
     ${media.greaterThan('medium')`
--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -2,6 +2,7 @@ import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Highlight from '.'
+import * as S from './styles'
 
 const props = {
   title: 'Heading 1',
@@ -37,5 +38,29 @@ describe('<Highlight />', () => {
   it('should render the float image', () => {
     renderWithTheme(<Highlight {...props} floatImage="/float-image.png" />)
     expect(screen.getByRole('img')).toHaveAttribute('src', '/float-image.png')
+  })
+
+  it('should align right by default', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'floatImage content'"
+    )
+
+    expect(container.firstChild).toHaveStyleRule('text-align', 'right', {
+      modifier: `${S.Content}`
+    })
+  })
+
+  it(`should align left if alignment="left" is set`, () => {
+    const { container } = renderWithTheme(
+      <Highlight {...props} alignment="left" />
+    )
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'content floatImage'"
+    )
   })
 })
