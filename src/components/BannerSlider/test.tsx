7f9a539a1f721d46e27298c692e3d6da8cf4cec3
Angelilton Epifanio <epifanio.rds@gmail.com>
2021-06-09 13:57:13 -0400

    Add BannerSlider with test

---
+++ src/components/BannerSlider/test.tsx
@@ -0,0 +1,49 @@
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import BannerSlider from '.'
+
+const items = [
+    {
+        img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+        title: 'Defy death 1',
+        subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+        buttonLabel: 'Buy now',
+        buttonLink: '/games/defy-death',
+        ribbon: 'Bestselling'
+    },
+    {
+        img: 'https://source.unsplash.com/user/willianjusten/1042x582',
+        title: 'Defy death 2',
+        subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+        buttonLabel: 'Buy now',
+        buttonLink: '/games/defy-death'
+    }
+]
+
+describe('<BannerSlider />', () => {
+    it('should render Vertical slider', () => {
+        const { container } = renderWithTheme(<BannerSlider items={items} />)
+
+        expect(container.querySelector('.slick-vertical')).toBeInTheDocument()
+    })
+
+    it('should render with 1 active item', () => {
+        renderWithTheme(<BannerSlider items={items} />)
+
+        expect(
+            screen.getByRole('heading', {
+                name: /defy death 1/i,
+                hidden: false
+            })
+        ).toBeInTheDocument()
+
+        expect(
+            screen.getByRole('heading', {
+                name: /defy death 2/i,
+                hidden: true
+            })
+        ).toBeInTheDocument()
+    })
+})
