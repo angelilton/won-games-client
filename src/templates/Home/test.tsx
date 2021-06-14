0e838c64c7596443acb332cb504ccbd398796524
Angelilton Epifanio <epifanio.rds@gmail.com>
2021-06-14 14:23:16 -0400

    add home sections

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -4,9 +4,28 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import Home from '.'
 
 describe('<Home />', () => {
-  it('should render the heading', () => {
+  it('should render menu and footer', () => {
     renderWithTheme(<Home />)
 
-    expect(screen.getByRole('heading', { name: /Home/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /follow us/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the sections', () => {
+    renderWithTheme(<Home />)
+
+    expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /most popular/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /upcomming/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /free games/i })
+    ).toBeInTheDocument()
   })
 })
