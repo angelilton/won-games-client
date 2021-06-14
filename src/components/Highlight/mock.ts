86b74169ecc92e50d922bf147540129f90d813eb
Angelilton Epifanio <epifanio.rds@gmail.com>
2021-06-14 14:50:15 -0400

    Add mocks

---
+++ src/components/Highlight/mock.ts
@@ -0,0 +1,7 @@
+export default {
+  title: 'Read Dead it’s back',
+  subtitle: 'Come see John’s new adventures',
+  backgroundImage: '/img/red-dead-img.jpeg',
+  buttonLabel: 'Buy now',
+  buttonLink: '/buyNow'
+}
--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -1,16 +1,12 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Highlight, { HighlightProps } from '.'
 
+import item from './mock'
+
 export default {
   title: 'Highlight',
   component: Highlight,
-  args: {
-    title: 'Read Dead it’s back',
-    subtitle: 'Come see John’s new adventures',
-    backgroundImage: '/img/red-dead-img.jpeg',
-    buttonLabel: 'Buy now',
-    buttonLink: '/buyNow'
-  }
+  args: { ...item }
 } as Meta
 
 export const Default: Story<HighlightProps> = (args) => <Highlight {...args} />
