8347d1859d33811919dff7c73e0c1fe7950658b9
Angelilton Epifanio <epifanio.rds@gmail.com>
2021-06-16 17:09:58 -0400

    Home add props to tests

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,11 +1,28 @@
+import 'match-media-mock'
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
+import bannersMock from 'components/BannerSlider/mock'
+import cardsMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
 import Home from '.'
+
+const props = {
+  banners: bannersMock,
+  newGames: cardsMock,
+  mostPopularHighLight: highlightMock,
+  mostPopularCards: cardsMock,
+  upcommingCards: cardsMock,
+  upcommingHighLight: highlightMock,
+  upcommingMoreCards: cardsMock,
+  freeCards: cardsMock,
+  freeHighLight: highlightMock
+}
 
 describe('<Home />', () => {
   it('should render menu and footer', () => {
-    renderWithTheme(<Home />)
+    renderWithTheme(<Home {...props} />)
 
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
 
@@ -15,7 +32,7 @@ describe('<Home />', () => {
   })
 
   it('should render the sections', () => {
-    renderWithTheme(<Home />)
+    renderWithTheme(<Home {...props} />)
 
     expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
     expect(
