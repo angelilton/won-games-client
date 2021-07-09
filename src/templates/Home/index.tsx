4b6c81c49de1d1330ea87dd5af2efaed79f482f3
Angelilton Epifanio <epifanio.rds@gmail.com>
2021-07-09 16:26:37 -0400

    refactor Home to use Base and Showcase

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,15 +1,13 @@
 import { BannerProps } from 'components/Banner'
-import Highlight, { HighlightProps } from 'components/Highlight'
+import { HighlightProps } from 'components/Highlight'
 import { GameCardProps } from 'components/GameCard'
 
 import Container from 'components/Container'
-import Footer from 'components/Footer'
-import Heading from 'components/Heading'
-import Menu from 'components/Menu'
 import BannerSlider from 'components/BannerSlider'
-import GameCardSlider from 'components/GameCardSlider'
 
 import * as S from './styles'
+import Showcase from 'components/Showcase'
+import Base from 'templates/Base'
 
 export type HomeTempleteProps = {
   banners: BannerProps[]
@@ -34,60 +32,30 @@ const Home = ({
   freeHighLight,
   freeCards
 }: HomeTempleteProps) => (
-  <section>
+  <Base>
     <Container>
-      <Menu />
       <S.SectionBanner>
         <BannerSlider items={banners} />
       </S.SectionBanner>
     </Container>
 
     <S.SectionNews>
-      <Container>
-        <Heading lineLeft lineColor="secondary" color="black">
-          News
-        </Heading>
-
-        <GameCardSlider items={newGames} color="black" />
-      </Container>
+      <Showcase title="News" games={newGames} />
     </S.SectionNews>
 
-    <Container>
-      <S.SectionMostPopular>
-        <Heading lineLeft lineColor="secondary">
-          Most Popular
-        </Heading>
+    <Showcase
+      title="Most Popular"
+      highlight={mostPopularHighLight}
+      games={mostPopularCards}
+    />
 
-        <Highlight {...mostPopularHighLight} />
-        <GameCardSlider items={mostPopularCards} />
-      </S.SectionMostPopular>
+    <S.SectionUpcoming>
+      <Showcase title="Upcomming" games={upcommingCards} />
+      <Showcase highlight={upcommingHighLight} games={upcommingMoreCards} />
+    </S.SectionUpcoming>
 
-      <S.SectionUpcoming>
-        <Heading lineLeft lineColor="secondary">
-          Upcomming
-        </Heading>
-
-        <GameCardSlider items={upcommingCards} />
-        <Highlight {...upcommingHighLight} />
-        <GameCardSlider items={upcommingMoreCards} />
-      </S.SectionUpcoming>
-
-      <S.SectionFreeGames>
-        <Heading lineLeft lineColor="secondary">
-          Free Games
-        </Heading>
-
-        <Highlight {...freeHighLight} />
-        <GameCardSlider items={freeCards} />
-      </S.SectionFreeGames>
-    </Container>
-
-    <S.SectionFooter>
-      <Container>
-        <Footer />
-      </Container>
-    </S.SectionFooter>
-  </section>
+    <Showcase title="Free Games" highlight={freeHighLight} games={freeCards} />
+  </Base>
 )
 
 export default Home
--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -3,74 +3,19 @@ import media from 'styled-media-query'
 
 import * as HeadingStyles from 'components/Heading/styles'
 import * as HighlightStyles from 'components/Highlight/styles'
-import * as GameCardSliderStyles from 'components/GameCardSlider/styles'
 
 export const SectionBanner = styled.section`
   ${({ theme }) => css`
-    margin: ${theme.spacings.large} calc(-${theme.grid.gutter} / 2);
-
+    margin: ${theme.spacings.small} calc(-${theme.grid.gutter} / 2);
     ${media.greaterThan('medium')`
-     margin: ${theme.spacings.large} 0;
+     margin: ${theme.spacings.small} 0;
      position: relative;
       z-index: ${theme.layers.base};
     `}
   `}
 `
 
-export const SectionFooter = styled.section`
-  ${({ theme }) => css`
-    margin-top: ${theme.spacings.large};
-    padding-top: ${theme.spacings.xxlarge};
-    padding-bottom: ${theme.spacings.xsmall};
-    background-color: ${theme.colors.white};
-
-    clip-path: polygon(0 5%, 100% 0%, 100% 100%, 0 100%);
-
-    ${media.greaterThan('medium')`
-      padding-top: calc(${theme.spacings.xxlarge} * 2);
-      clip-path: polygon(0 15%, 100% 0%, 100% 100%, 0 100%);
-    `}
-  `}
-`
-const Sections = styled.section`
-  ${({ theme }) => css`
-    ${HeadingStyles.Wrapper},
-    ${HighlightStyles.Wrapper},
-    ${GameCardSliderStyles.Wrapper} {
-      margin-bottom: ${theme.spacings.medium};
-    }
-
-    //remove gutter from highLight when it's mobile
-    ${HighlightStyles.Wrapper} {
-      ${media.lessThan('medium')`
-        margin-right: calc(-${theme.grid.gutter} / 2);
-        margin-left: calc(-${theme.grid.gutter} / 2);
-      `}
-    }
-
-    //remove margin right from cards
-    ${GameCardSliderStyles.Wrapper} {
-      ${media.lessThan('huge')`
-        margin-right: calc(-${theme.grid.gutter} / 2);
-      `}
-    }
-
-    margin-bottom: calc(${theme.spacings.large} * 2);
-  `}
-`
-export const SectionMostPopular = styled(Sections)``
-
-export const SectionFreeGames = styled(Sections)``
-
-export const SectionUpcoming = styled(Sections)`
-  ${({ theme }) => css`
-    ${HighlightStyles.Wrapper} {
-      margin-top: calc(${theme.spacings.xlarge} * 2);
-    }
-  `}
-`
-
-export const SectionNews = styled(Sections)`
+export const SectionNews = styled.div`
   ${({ theme }) => css`
     margin-bottom: calc(${theme.spacings.xxlarge} * 2);
 
@@ -86,5 +31,21 @@ export const SectionNews = styled(Sections)`
       clip-path: polygon(0 0, 100% 15%, 100% 100%, 0 85%);
 
     `}
+
+    ${HeadingStyles.Wrapper} {
+      color: ${theme.colors.black};
+    }
+    .slick-prev,
+    .slick-next {
+      color: ${theme.colors.black};
+    }
+  `}
+`
+
+export const SectionUpcoming = styled.div`
+  ${({ theme }) => css`
+    ${HighlightStyles.Wrapper} {
+      margin-top: calc(${theme.spacings.xlarge} * 2);
+    }
   `}
 `
