d0010ef7153f82133271e9a1c810acf315213411
Angelilton Epifanio <epifanio.rds@gmail.com>
2021-07-13 17:50:03 -0400

    Add GameDetails

---
+++ src/components/GameDetails/index.tsx
@@ -0,0 +1,86 @@
+import Heading from 'components/Heading'
+import { Apple, Linux, Windows } from 'styled-icons/fa-brands'
+import * as S from './styles'
+
+type Platform = 'windows' | 'linux' | 'mac'
+type Rating = 'BR0' | 'BR10' | 'BR12' | 'BR14' | 'BR16' | 'BR18'
+
+export type GameDetailsProps = {
+  developer: string
+  publisher: string
+  platforms: Platform[]
+  releaseDate: string
+  rating: Rating
+  genres: string[]
+}
+
+const DateFormat = (date: string) =>
+  new Intl.DateTimeFormat('en-US', {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric'
+  }).format(new Date(date))
+
+const GameDetails = ({
+  platforms,
+  developer,
+  publisher,
+  releaseDate,
+  rating,
+  genres
+}: GameDetailsProps) => {
+  const platformIcons = {
+    linux: <Linux title="Linux" size={18} />,
+    mac: <Apple title="Mac" size={18} />,
+    windows: <Windows title="Windows" size={18} />
+  }
+
+  return (
+    <S.Wrapper>
+      <Heading lineLeft lineColor="secondary">
+        Games Details
+      </Heading>
+
+      <S.Content>
+        <S.Block>
+          <S.Label>Developer</S.Label>
+          <S.Description>{developer}</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Release Date</S.Label>
+          <S.Description>{DateFormat(releaseDate)}</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Platforms</S.Label>
+          <S.IconWrapper>
+            {platforms.map((icon: Platform) => (
+              <S.Icon key={icon}>{platformIcons[icon]}</S.Icon>
+            ))}
+          </S.IconWrapper>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Publisher</S.Label>
+          <S.Description>{publisher}</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Rating</S.Label>
+          <S.Description>
+            {rating === 'BR0' ? 'FREE' : `${rating.replace('BR', '')}+`}
+          </S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Genres</S.Label>
+          {genres.map((item) => (
+            <S.Description key={item}>{item}</S.Description>
+          ))}
+        </S.Block>
+      </S.Content>
+    </S.Wrapper>
+  )
+}
+export default GameDetails
