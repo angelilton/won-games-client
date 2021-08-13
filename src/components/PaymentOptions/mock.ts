a007c3398604ff9ab3b174700d7b66e3beeb9ce7
Angelilton Epifanio <epifanio.rds@gmail.com>
2021-08-12 20:10:11 -0400

    Add PaymentOptions

---
+++ src/components/PaymentOptions/mock.ts
@@ -0,0 +1,10 @@
+export default [
+  {
+    flag: 'visa',
+    number: '*** *** **** 4325'
+  },
+  {
+    flag: 'mastercard',
+    number: '*** *** **** 4326'
+  }
+]
---
+++ src/components/PaymentOptions/stories.tsx
@@ -0,0 +1,26 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import PaymentOptions, { PaymentOptionsProps } from '.'
+
+import cardsMock from './mock'
+
+export default {
+  title: 'Cart/PaymentOptions',
+  component: PaymentOptions,
+  args: {
+    cards: cardsMock
+  },
+  argTypes: {
+    cards: {
+      type: ''
+    },
+    handlePayment: {
+      action: 'clicked'
+    }
+  }
+} as Meta
+
+export const Default: Story<PaymentOptionsProps> = (args) => (
+  <div style={{ padding: 16, maxWidth: 400 }}>
+    <PaymentOptions {...args} />
+  </div>
+)
