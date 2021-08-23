a80c204da37066d7352dc0e3c866da28c735f605
Angelilton Epifanio <epifanio.rds@gmail.com>
2021-08-23 12:37:03 -0400

    Add useRouter to activeLink and test

--- src/templates/Profile/test.tsx
+++ src/templates/Profile/test.tsx
@@ -1,6 +1,26 @@
+import 'match-media-mock'
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
+
 import Profile from '.'
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(() => ({ asPath: '/profile/me' }))
+}))
+
+jest.mock('components/Heading', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock Heading">{children}</div>
+  }
+}))
+
+jest.mock('components/ProfileMenu', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="ProfileMenu" />
+  }
+}))
 
 describe('<Profile />', () => {
   it('should render profile with ProfileMenu and children', () => {
@@ -10,8 +30,8 @@ describe('<Profile />', () => {
       </Profile>
     )
 
-    expect(screen.getByRole('heading', { name: /my profile/i }))
-    expect(screen.getByText('My orders'))
-    expect(screen.getByText('cards List'))
+    expect(screen.getByText('cards List')).toBeInTheDocument()
+    expect(screen.getByText('My profile')).toBeInTheDocument()
+    expect(screen.getByTestId('ProfileMenu')).toBeInTheDocument()
   })
 })
