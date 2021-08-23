1105ed4c03bd18179664e6ca0f5ab163319728f8
Angelilton Epifanio <epifanio.rds@gmail.com>
2021-08-23 11:53:53 -0400

    add page profile/orders

---
+++ src/pages/profile/orders.tsx
@@ -0,0 +1,21 @@
+import OrdersList, { OrdersListProps } from 'components/OrdersList'
+
+import Profile from 'templates/Profile'
+
+import ordersMock from 'components/OrdersList/mock'
+
+export default function index({ items }: OrdersListProps) {
+  return (
+    <Profile>
+      <OrdersList items={items} />
+    </Profile>
+  )
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      items: ordersMock
+    }
+  }
+}
