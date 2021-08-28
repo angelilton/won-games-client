ce6dfb84286268c67f936d1ddbe1900bffff5422
Angelilton Epifanio <epifanio.rds@gmail.com>
2021-08-27 20:01:46 -0400

    Add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+import Empty from 'components/Empty'
+import Base from 'templates/Base'
+
+const Page404 = () => (
+  <Base>
+    <Empty
+      hasLink
+      title="404: Not found"
+      description="Ops...this page does not exist. Go back to the store and enjoy our offers."
+    />
+  </Base>
+)
+
+export default Page404
