08125b92d0463e9ca0ca85844d2da2015b60aa1e
Angelilton Epifanio <epifanio.rds@gmail.com>
2021-09-16 10:17:30 -0400

    Add ApolloProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,32 @@
+import { ApolloProvider } from '@apollo/client'
 import { AppProps } from 'next/app'
-import Head from 'next/head'
+import { useApollo } from 'utils/apollo'
 import { ThemeProvider } from 'styled-components'
 
+import Head from 'next/head'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
+  const client = useApollo(pageProps.initialApoloState)
+
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Won games Store</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-        />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Won games Store</title>
+          <link rel="shortcut icon" href="/img/icon-512.png" />
+          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="description"
+            content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          />
+        </Head>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 
