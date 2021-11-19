7ec4d19225a2d9fc60236347cc63dd990ea14cf8
angelilton <epifanio.rds@gmail.com>
2021-11-19 16:48:49 -0400

    Add mappers test

--- src/utils/mappers/index.ts
+++ src/utils/mappers/index.ts
@@ -3,8 +3,7 @@ import {
   QueryHome_banners,
   QueryHome_sections_freeGames_highlight
 } from 'graphql/generated/QueryHome'
-
-import getImageUrl from './getImageUrl'
+import getImageUrl from 'utils/getImageUrl'
 
 export const bannerMapper = (banners: QueryHome_banners[]) => {
   return banners.map(({ image, title, subtitle, button, ribbon }) => ({
@@ -22,30 +21,29 @@ export const bannerMapper = (banners: QueryHome_banners[]) => {
 }
 
 export const gamesMapper = (games: QueryGames_games[] | null | undefined) => {
-  return (
-    games &&
-    games.map(({ name, slug, developers, cover, price }) => ({
-      slug,
-      price,
-      tittle: name,
-      developer: developers[0].name,
-      img: getImageUrl(cover?.url)
-    }))
-  )
+  return games
+    ? games.map(({ name, slug, developers, cover, price }) => ({
+        slug,
+        price,
+        title: name,
+        developer: developers[0].name,
+        img: getImageUrl(cover?.url)
+      }))
+    : []
 }
 
 export const highlightMapper = (
   highlight: QueryHome_sections_freeGames_highlight | null | undefined
 ) => {
-  return (
-    highlight && {
-      title: highlight.title,
-      subtitle: highlight.subtitle,
-      backgroundImage: highlight.background?.url,
-      floatImage: highlight.floatImage?.url,
-      buttonLabel: highlight.buttonLabel,
-      buttonLink: highlight.buttonLink,
-      alignment: highlight.alignment
-    }
-  )
+  return highlight
+    ? {
+        title: highlight.title,
+        subtitle: highlight.subtitle,
+        backgroundImage: highlight.background?.url,
+        floatImage: highlight.floatImage?.url,
+        buttonLabel: highlight.buttonLabel,
+        buttonLink: highlight.buttonLink,
+        alignment: highlight.alignment
+      }
+    : {}
 }
