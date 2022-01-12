31fe747e7244af01ea2226cbeb0091a5e4ece460
Angelilton <epifanio.rds@gmail.com>
2022-01-12 12:18:22 -0400

    Configure NextAuth and Add AuthProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { SessionProvider as AuthProvider } from 'next-auth/react'
 import { ApolloProvider } from '@apollo/client'
 import { ThemeProvider } from 'styled-components'
 import { CartProvider } from 'hooks/use-cart'
@@ -13,34 +14,36 @@ function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
 
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <CartProvider>
-          <Head>
-            <title>Won games Store</title>
-            <link rel="shortcut icon" href="/img/icon-512.png" />
-            <link rel="apple-touch-icon" href="/img/icon-512.png" />
-            <link rel="manifest" href="/manifest.json" />
-            <meta
-              name="description"
-              content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+    <AuthProvider session={pageProps.session}>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <CartProvider>
+            <Head>
+              <title>Won games Store</title>
+              <link rel="shortcut icon" href="/img/icon-512.png" />
+              <link rel="apple-touch-icon" href="/img/icon-512.png" />
+              <link rel="manifest" href="/manifest.json" />
+              <meta
+                name="description"
+                content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+              />
+              <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1"
+              />
+            </Head>
+            <GlobalStyles />
+            <NextNProgress
+              color="#F231A5"
+              startPosition={0.3}
+              stopDelayMs={200}
+              height={5}
             />
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-          </Head>
-          <GlobalStyles />
-          <NextNProgress
-            color="#F231A5"
-            startPosition={0.3}
-            stopDelayMs={200}
-            height={5}
-          />
-          <Component {...pageProps} />
-        </CartProvider>
-      </ThemeProvider>
-    </ApolloProvider>
+            <Component {...pageProps} />
+          </CartProvider>
+        </ThemeProvider>
+      </ApolloProvider>
+    </AuthProvider>
   )
 }
 
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,56 @@
+import NextAuth, { NextAuthOptions } from 'next-auth'
+import { NextApiRequest, NextApiResponse } from 'next'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+const options: NextAuthOptions = {
+  pages: {
+    signIn: '/sign-in'
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Sing-in',
+      credentials: {},
+      async authorize(credentials) {
+        console.log('credencial:', credentials)
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/auth/local`,
+          {
+            method: 'POST',
+            body: new URLSearchParams({
+              identifier: JSON.stringify(credentials)
+            })
+          }
+        )
+
+        const data = await response.json()
+
+        if (data.user) {
+          return { ...data.user, jwt: data.jwt }
+        } else {
+          return null
+        }
+      }
+    })
+  ],
+  callbacks: {
+    session: async ({ session, user }) => {
+      session.jwt = user.jwt
+      session.id = user.id
+
+      return Promise.resolve(session)
+    },
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.id = user.id
+        token.email = user.email
+        token.name = user.name
+        token.jwt = user.jwt
+      }
+
+      return Promise.resolve(token)
+    }
+  }
+}
+
+export default (req: NextApiRequest, res: NextApiResponse) =>
+  NextAuth(req, res, options)
