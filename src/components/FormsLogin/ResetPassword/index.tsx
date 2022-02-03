944188a2ec3cd09d3c3ce6d5c8b95edff324affa
Angelilton <epifanio.rds@gmail.com>
2022-02-03 19:10:23 -0400

    add Reset Password flow

--- src/components/FormsLogin/ResetPassword/index.tsx
+++ src/components/FormsLogin/ResetPassword/index.tsx
@@ -1,9 +1,101 @@
-import * as S from './styles'
+import { useState } from 'react'
+import Button from 'components/Button'
+import TextField from 'components/TextField'
+import { FieldErrors, resetValidate } from 'utils/validations'
+import { ButtonLoading } from 'components/Button/styles'
+import { FormErrorMessage, FormWrapper } from '../styles'
+import { ErrorOutline, Lock } from 'styled-icons/material-outlined'
+import { useRouter } from 'next/router'
+import { signIn } from 'next-auth/react'
 
-const FormResetPassword = () => (
-  <S.Wrapper>
-    <h1>FormResetPassword</h1>
-  </S.Wrapper>
-)
+const FormResetPassword = () => {
+  const [formError, setFormError] = useState('')
+  const [fieldError, setFieldError] = useState<FieldErrors>({})
+  const [values, setValues] = useState({ password: '', confirm_password: '' })
+  const [loading, setLoading] = useState(false)
+  const { query } = useRouter()
+
+  const handleInput = (field: string, value: string) => {
+    setValues((s) => ({ ...s, [field]: value }))
+  }
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+    //loading
+    setLoading(true)
+
+    //validate fields
+    const errors = resetValidate(values)
+
+    if (Object.keys(errors).length) {
+      setFieldError(errors)
+      setLoading(false)
+      return
+    }
+
+    setFieldError({})
+
+    //send post to api
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/reset-password`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          code: query.code,
+          password: values.password,
+          passwordConfirmation: values.confirm_password
+        })
+      }
+    )
+
+    const data = await response.json()
+    setLoading(false)
+
+    if (data.error) {
+      setFormError(data.message[0].messages[0].message)
+    } else {
+      signIn('credentials', {
+        identifier: data.user.email,
+        password: values.password,
+        callbackUrl: '/'
+      })
+    }
+  }
+
+  return (
+    <FormWrapper>
+      {!!formError && (
+        <FormErrorMessage>
+          <ErrorOutline /> {formError}
+        </FormErrorMessage>
+      )}
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name="password"
+          placeholder="Password"
+          type="password"
+          error={fieldError?.password}
+          onInputChange={(v) => handleInput('password', v)}
+          icon={<Lock />}
+        />
+        <TextField
+          name="confirm_password"
+          placeholder="Confirm password"
+          type="password"
+          error={fieldError?.confirm_password}
+          onInputChange={(v) => handleInput('confirm_password', v)}
+          icon={<Lock />}
+        />
+
+        <Button type="submit" size="large" fullWidth disabled={loading}>
+          {loading ? <ButtonLoading /> : <span>Reset Password</span>}
+        </Button>
+      </form>
+    </FormWrapper>
+  )
+}
 
 export default FormResetPassword
