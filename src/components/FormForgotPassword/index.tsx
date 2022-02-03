7158b6e87d6a9275b5529edeb57595a578b52189
Angelilton <epifanio.rds@gmail.com>
2022-02-03 11:26:35 -0400

    Add Forgot-password and reset-password page flow

---
+++ src/components/FormForgotPassword/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const FormForgotPassword = () => (
+  <S.Wrapper>
+    <h1>FormForgotPassword</h1>
+  </S.Wrapper>
+)
+
+export default FormForgotPassword
---
+++ src/components/FormResetPassword/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const FormResetPassword = () => (
+  <S.Wrapper>
+    <h1>FormResetPassword</h1>
+  </S.Wrapper>
+)
+
+export default FormResetPassword
